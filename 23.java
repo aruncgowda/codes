223abc91990128b52ffc71c8b3bdb83b8e9e8974
ARUN C <64681005+aruncgowda@users.noreply.github.com>
2021-07-24 12:38:11 +0530

    Add files via upload

---
+++ 23.java
@@ -0,0 +1,12 @@
+ public class ApplicationTester 
+ { 
+   public static void main(String[] args)
+   {
+     int i = 0; 
+     do
+     { 
+       System.out.println("Hello");
+       i++; 
+     } while(i <= 1); 
+   } 
+ }
